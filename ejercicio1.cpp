d4ae0ddd3e62bbcf5b238d492f914feda482bda3
Evelyn-Michelle <00019425@uca.edu.sv>
2025-05-29 13:01:30 -0600

    tarea

---
+++ ejercicio1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ int edad;
+
+ cout <<"ingrese su edad";
+ cin >> edad;
+ 
+ if(edad>=18){
+    cout<<"es mayor de edad";
+ }else if( edad<18){
+    cout <<" es menor de edad";
+ }
+return 0;
+
+}
---
+++ ejercicio5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int num, par, impar;
+
+cout <<" ingrese un numero";
+
+cin >>num;
+
+if(num%2==0){
+
+cout<< num <<" es un numero par"<< endl;
+} else {
+    cout<< num << " es un numjero impar"<< endl;
+
+}
+
+return 0;
+}
